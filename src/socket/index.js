8a7914b394199179ef3122c9e60dec330dc044c9
minaabskhron <minaabskhron@gmail.com>
2025-05-24 20:01:56 +0300

    remove the conversation from socket

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,9 +1,15 @@
 import messageSocket from "./message.socket.js";
 
 export function registerSocketHandlers(io) {
+  // 1) io.on("connection", …)
+  //    – Fires once **per new client** that connects
+  //    – Gives you a `socket` object you can use for that client
   io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
 
+    // 2) socket.on("join", …)
+    //    – Fires when this particular client emits “join”
+    //    – Lets you handle custom events from that one client
     socket.on("join", (userId) => {
       socket.join(userId);
       console.log(`…socket ${socket.id} joined room ${userId}`);
--- src/socket/message.socket.js
+++ src/socket/message.socket.js
@@ -3,19 +3,21 @@ import { createMessage } from "../modules/message/message.service.js";
 export default function messageSocket(io, socket) {
   socket.on("send-message", async ({ senderId, receiverId, text }) => {
     try {
-      const { populatedMessage, updatedConversation } = await createMessage({
+      const { populatedMessage } = await createMessage({
         senderId,
         receiverId,
         text,
       });
       // Emit to sender and receiver rooms (you’ll need to have them join)
+      // 1) Send the new message back to the sender’s own room:
+
       io.to(senderId).emit("new-message", {
         populatedMessage,
-        updatedConversation,
       });
+      // 2) Send the new message to the receiver’s room:
+
       io.to(receiverId).emit("new-message", {
         populatedMessage,
-        updatedConversation,
       });
     } catch (err) {
       socket.emit("message-error", err.message);
